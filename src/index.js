44cc8c13b75251a6b7986e20b39e39645cf3335e
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-03-17 23:42:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import searchImages from './js/searchImages.js';
-import loadMore from './js/loadMore.js';
+// import searchImages from './js/searchImages.js';
+// import loadMore from './js/loadMore.js';
+import { searchImages, loadMore } from './js/searchImages.js';
 import refs from './js/refs.js';
 import './styles.css';
 
