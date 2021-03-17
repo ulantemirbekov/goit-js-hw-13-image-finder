780cc8187dd477776a9e14ce43dcc56160faf6cd
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-03-17 22:24:32 +0200

    Update searchImages.js

--- src/js/searchImages.js
+++ src/js/searchImages.js
@@ -1,13 +1,12 @@
 import fetchImages from './apiService.js';
 import addToMarkup from './addToMarkup.js';
-import refs from './refs.js'
-
+import refs from './refs.js';
 
 let inputValue = '';
 let page = 1;
-refs.btnLoadRef.style.display = 'none';
+refs.btnLoadMoreRef.style.display = 'none';
 
-export const searchImages = (event) => {
+const searchImages = (event) => {
     event.preventDefault();
     refs.galleryRef.innerHTML = '';
     inputValue = event.target.elements.query.value;
@@ -16,21 +15,10 @@ export const searchImages = (event) => {
         fetchImages(inputValue, page)
             .then(images => {
                 addToMarkup(images)
-                refs.btnLoadRef.style.display = 'block';
+                refs.btnLoadMoreRef.style.display = 'block';
             })
             .catch(error => console.log(error))
-    };
+    }
 };
 
-export const loadMore = () => {
-    page += 1;
-
-    fetchImages(inputValue, page)
-        .then(images => {
-            addToMarkup(images)
-            window.scrollTo({
-                top: document.documentElement.offsetHeight,
-                behavior: 'smooth'
-            });
-        }).catch(error => console.log(error))
-};
+export default searchImages;
