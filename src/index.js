fa9511fb537183bb6d4a08de45dc861b44ad4e98
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-03-17 22:34:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { searchImages } from './js/searchImages.js';
-import { loadMore } from './js/searchImages.js';
+import searchImages from './js/searchImages.js';
+import loadMore from './js/loadMore.js';
 import refs from './js/refs.js';
 import './styles.css';
 
 refs.formRef.addEventListener('submit', searchImages);
-refs.btnLoadRef.addEventListener('click', loadMore);
+refs.btnLoadMoreRef.addEventListener('click', loadMore);
